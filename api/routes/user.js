eb4af06a8f0ef9d9d44b3dd588faeb6a300d5add
Fredrik <fredrik@Fredriks-MacBook-Pro.local>
2018-07-25 12:07:43 +0200

    test

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,9 +7,7 @@
 // router.post("/signup", UserController.user_signup);
 
 // router.post("/login", UserController.user_login);
-
 // router.delete("/:userId", checkAuth, UserController.user_delete);
-
 // module.exports = router;
 
 const express = require("express");
