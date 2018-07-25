6d707da257634d66a9f9914b455b0290b093dd11
Fredrik <fredrik@Fredriks-MacBook-Pro.local>
2018-07-25 11:35:51 +0200

    Initial Commit

---
+++ app.js
@@ -0,0 +1,67 @@
+  const express = require("express");
+  const app = express();
+  const morgan = require("morgan");
+  const bodyParser = require("body-parser");
+  const mongoose = require("mongoose");
+  
+  const productRoutes = require("./api/routes/products");
+  const orderRoutes = require("./api/routes/orders");
+  const userRoutes = require('./api/routes/user');
+
+  
+  // var MongoClient = require('mongodb').MongoClient;
+  // var uri = "mongodb://mongodb-stitch-newapp-nrylz:<PASSWORD>@cluster0-shard-00-00-k5bsb.mongodb.net:27017,cluster0-shard-00-01-k5bsb.mongodb.net:27017,cluster0-shard-00-02-k5bsb.mongodb.net:27017/admin?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
+  // MongoClient.connect(uri, function(err, db) {
+  //    db.close();
+  // });
+ 
+mongoose.connect(
+  "mongodb+srv://mongouser:" +
+       process.env.MONGO_ATLAS_PW +"@cluster0-k5bsb.mongodb.net/test?retryWrites=true" ,
+  {
+    //useMongoClient: true,
+    useNewUrlParser: true
+  }
+);
+  mongoose.Promise = global.Promise;
+  
+
+app.use(morgan("dev"));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
+
+// Routes which should handle requests
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/user", userRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
